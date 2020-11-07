3ae89c5d1cd6640ca2c89c0c8e1862a635a250ef
Ammar Bin Nasir <majidammar428@gmail.com>
2020-11-07 12:41:23 +0500

    Setting up Database
    --- Setting up ---
    
    - Download MySQL and set up a connection
    
    - Connect to the MySQL connection using the host, username and password.
    
    - Set up a Cursor
    
     -- (WHAT IS CURSOR?
    
    -The MySQLCursor class instantiates objects that can execute operations such as SQL statements. Cursor objects interact with the MySQL server using a MySQLConnection object. ) --
    
    - Create Databse using <cursor_name>.execute("CREATE DATABASE <db_name")
    
    - Run this code once and make sure theres no error
    
    - Remove lines commented with "Run once" and add the line:
    database = "<db_name>" to the connector.

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import mysql.connector
 
-
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="project1"
+  password="project1",
+  database="projdatabase" # ONLY add this line after the database has been CREATED!
 )
 
-print(mydb)
+
+dbCursor = mydb.cursor()
+
+#### -- CREATE DATABASE -- ####
+# # dbCursor.execute("CREATE DATABASE projadatabase")  # Run once
+
+#### -- CHECK TO SEE IF DB EXISTS -- ####
+# dbCursor.execute("SHOW DATABASES")  # Run in conjunction with the for loop below
+# for x in dbCursor:  # prints information about the DB.
+#     print(x)
